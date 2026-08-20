c552f2d5bdd6ee570324a4b2785c0b634a8aa134
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-08-20 20:06:14 +0200

    admin wip

--- Tests/Support/Algebra Modular Primitives Test Support.swift
+++ Tests/Support/Algebra Modular Primitives Test Support.swift
@@ -1,4 +1,4 @@
 // Algebra Modular Primitives Test Support
 // Provides test-only conveniences for algebra-modular-primitives.
 
-public import Algebra_Modular_Primitives
+import Algebra_Modular_Primitives
